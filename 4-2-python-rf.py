4f6b0b498ac350301f9a94a6b4ff63cde474c7f1
dduniverse <suhyunbory@gmail.com>
2022-12-02 17:39:39 +0900

    Kaggle Notebook |  [4회 기출 유형 | 작업형2 | Python] RF | Version 2

---
+++ 4-2-python-rf.py
@@ -0,0 +1,67 @@
+# This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+# Input data files are available in the read-only "../input/" directory
+# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
+
+import os
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+# You can write up to 20GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All" 
+# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
+
+train=pd.read_csv('/kaggle/input/big-data-analytics-certification-kr-2022/train.csv')
+test=pd.read_csv('/kaggle/input/big-data-analytics-certification-kr-2022/test.csv')
+
+id=test['ID']
+x_train=train.drop(columns=['ID','Segmentation'])
+x_test=test.drop(columns=['ID'])
+y_train=train['Segmentation']
+
+# print(x_train.isnull().sum()) # 결측치 없음
+# print(x_test.isnull().sum())
+
+
+from sklearn.preprocessing import LabelEncoder
+col=['Gender','Ever_Married','Graduated','Profession','Spending_Score','Var_1']
+encoder=LabelEncoder()
+for i in col:
+    x_train[i]=encoder.fit_transform(x_train[i])
+    x_test[i]=encoder.transform(x_test[i])
+    x_train[i]=x_train[i].astype('category')
+    x_test[i]=x_test[i].astype('category')
+
+x_train=pd.get_dummies(x_train)
+x_test=pd.get_dummies(x_test)
+
+from sklearn.preprocessing import StandardScaler
+val=[['Age','Work_Experience','Family_Size']]
+scaler=StandardScaler()
+for i in val:
+    x_train[i]=scaler.fit_transform(x_train[i])
+    x_test[i]=scaler.transform(x_test[i])
+    
+from sklearn.model_selection import train_test_split
+X_train,X_valid,Y_train,Y_valid=train_test_split(x_train,y_train,test_size=0.2,random_state=10)
+
+from sklearn.ensemble import RandomForestClassifier
+model1=RandomForestClassifier()
+model1.fit(X_train,Y_train)
+pred1=model1.predict(X_valid)
+
+# from xgboost import XGBClassifier # ValueError: Invalid classes inferred from unique values of `y`. Expected: [0 1 2 3], got [1 2 3 4]
+# model2=XGBClassifier()
+# model2.fit(X_train,Y_train)
+# pred2=model2.predict(X_valid)
+
+# from sklearn.metrics import f1_score
+# print('RF',f1_score(Y_valid,pred1,average='macro')) # 0.4697
+
+result=model1.predict(x_test)
+pd.DataFrame({'ID':id,'Segmentation':result}).to_csv('submission.csv',index=False)
